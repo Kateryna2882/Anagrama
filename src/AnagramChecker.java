fe2c16c497b75543cf17c14de05c7c922a70512c
Kateryna2882 <govoryhina82@gmail.com>
2024-07-28 13:44:10 +0300

    Add Anagram class

--- src/AnagramChecker.java
+++ src/AnagramChecker.java
@@ -1,2 +1,26 @@
+import java.util.Arrays;
+
 public class AnagramChecker {
-}
+
+        public static boolean isAnagram(String s, String t) {
+            if (s.length() != t.length()) {
+                return false;
+            }
+
+            char[] sArray = s.toCharArray();
+            char[] tArray = t.toCharArray();
+
+            Arrays.sort(sArray);
+            Arrays.sort(tArray);
+
+            return Arrays.equals(sArray, tArray);
+        }
+
+        public static void main(String[] args) {
+            String s = "listen";
+            String t = "silent";
+
+            System.out.println(isAnagram(s, t));
+        }
+    }
+
